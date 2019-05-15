ec11ce582f21077905c5cc5ad807ffe0156c62c4
David Eastmond <davebuck1981@gmail.com>
2019-05-15 22:50:22 +0000

    removed excess comments in the app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -12,38 +12,31 @@ function createTweetElement (fromData) {
    /* takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.
   The tweet data object that the function will take will have all the necessary tweet data:
   */
-  // Create Tweet body (article)
+  
   const $tweet = $("<article>");
   $tweet.empty(); 
   $tweet.addClass("tweet");
 
-  // Inside the article, the header, add the classes
+ 
   const $header = $("<header>").addClass("header");
   $header.addClass("header-float");
 
-  // Twitter avatar image - set the source and add classes
+
   const $avatar = $("<img>");
   $avatar.addClass("avatar")
   $avatar.attr('src', fromData.user.avatars.small);
     
-  // display name
   const $displayName = $("<h2>", {text: `${fromData.user.name}`});
   $displayName.addClass("display-name");
   $displayName.addClass("tweet-name-hover");
 
-  // twitter handle
   const $twitterHandleAside = $("<aside>", {text: `${fromData.user.handle}`}).addClass("twitter-handle");
   $twitterHandleAside.addClass("twitter-handle-hover");
   
-  // Append the above elements to the header
   $header.append($avatar, $displayName, $twitterHandleAside);
-  // Get tweet text content
-  const $bodyText = $("<p>", {text: `${fromData.content.text}`}).addClass("tweet-text");
-  
-  // Footer
-  const $tweetFooter = $("<footer>").addClass("footer");
 
-  // Date-time
+  const $bodyText = $("<p>", {text: `${fromData.content.text}`}).addClass("tweet-text");
+  const $tweetFooter = $("<footer>").addClass("footer");
   const $dateTimeAgo = $("<a>", {text: `${fromData.created_at}`}).addClass("tweet-date");
 
   // Create a div w/ heart, retweet and flag icons and append to the footer
